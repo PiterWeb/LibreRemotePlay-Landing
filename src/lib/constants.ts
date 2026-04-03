4eb6b9eb06f0a5de4caa17bd89372f09687bee60
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2026-04-03 13:07:18 +0200

    wikiUrl codeberg

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,6 @@
 export const githubUrl = "https://github.com/PiterWeb/LibreRemotePlay";
+export const codebergUrl = "https://codeberg.org/PiterWeb/LibreRemotePlay"
 export const downloadUrl = `${githubUrl}/releases/latest/`;
 export const clientUrl = 'https://libreremoteplay-client.vercel.app';
 export const discordUrl = "https://discord.gg/v7qd7nU3fJ";
-export const wikiUrl = `${githubUrl}/wiki`;
+export const wikiUrl = `${codebergUrl}/wiki`;
